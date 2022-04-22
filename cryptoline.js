e4abc80c0184fe099e149fc426a19099dfcd222a
Endri <57300637+Endri2001@users.noreply.github.com>
2022-04-21 23:59:16 -0400

    cryptoline.js file
    code for cryptoline.js file

--- cryptoline.js
+++ cryptoline.js
@@ -1,39 +1,66 @@
 // append the svg object to the body of the page
-const height = 800;
-const width = 1200;
-var svg = d3.select("#crypto")
+var margin = {top: 10, right: 30, bottom: 30, left: 60}
+var width = 460 - margin.left - margin.right;
+var  height = 400 - margin.top - margin.bottom;
+var svg = d3.select("#my_dataviz")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
   .append("g")
     .attr("transform",
-          "translate(50,50)");
+          "translate(" + margin.left + "," + margin.top + ")");
 
 //Read the data
 d3.csv("Bitcoin.csv",
   // When reading the csv, I must format variables:
   function(d){
-    return { date : d3.timeParse("%m-%d-%Y")(d.Date), value : d.High }
+    //console.log(d)
+    // console.log(d.Date)
+    // console.log(d.Date)
+    // console.log(d3.timeParse("%m/%d/%Y")(d.Date))
+    return { date : d3.timeParse("%m/%d/%Y")(d.Date), value : d.High }
   },
 
-  // Now I can use this dataset:
-  function(data) {
+   // Now I can use this dataset:
+   function(data) {
+    console.log(data)
     // Add X axis --> it is a date format
     var x = d3.scaleTime()
       .domain(d3.extent(data, function(d) { return d.date; }))
       .range([ 0, width ]);
-    svg.append("g")
+    xAxis = svg.append("g")
       .attr("transform", "translate(0," + height + ")")
       .call(d3.axisBottom(x));
-    console.log(data)
 
     // Add Y axis
     var y = d3.scaleLinear()
       .domain([0, d3.max(data, function(d) { return +d.value; })])
       .range([ height, 0 ]);
-    svg.append("g")
+    yAxis = svg.append("g")
       .call(d3.axisLeft(y));
 
+    // Add a clipPath: everything out of this area won't be drawn.
+    var clip = svg.append("defs").append("svg:clipPath")
+        .attr("id", "clip")
+        .append("svg:rect")
+        .attr("width", width )
+        .attr("height", height )
+        .attr("x", 0)
+        .attr("y", 0);
+
+    // Add brushing
+    var brush = d3.brushX()                   // Add the brush feature using the d3.brush function
+        .extent( [ [0,0], [width,height] ] )  // initialise the brush area: start at 0,0 and finishes at width,height: it means I select the whole graph area
+        .on("end", updateChart)               // Each time the brush selection changes, trigger the 'updateChart' function
+
+    // Create the line variable: where both the line and the brush take place
+    var line = svg.append('g')
+      .attr("clip-path", "url(#clip)")
+
     // Add the line
-    svg.append("path")
+    line.append("path")
       .datum(data)
+      .attr("class", "line")  // I add the class line to be able to modify this line later on.
       .attr("fill", "none")
       .attr("stroke", "steelblue")
       .attr("stroke-width", 1.5)
@@ -41,5 +68,55 @@ d3.csv("Bitcoin.csv",
         .x(function(d) { return x(d.date) })
         .y(function(d) { return y(d.value) })
         )
+
+    // Add the brushing
+    line
+      .append("g")
+        .attr("class", "brush")
+        .call(brush);
+
+    // A function that set idleTimeOut to null
+    var idleTimeout
+    function idled() { idleTimeout = null; }
+
+    // A function that update the chart for given boundaries
+    function updateChart() {
+
+      // What are the selected boundaries?
+      extent = d3.event.selection
+
+      // If no selection, back to initial coordinate. Otherwise, update X axis domain
+      if(!extent){
+        if (!idleTimeout) return idleTimeout = setTimeout(idled, 350); // This allows to wait a little bit
+        x.domain([ 4,8])
+      }else{
+        x.domain([ x.invert(extent[0]), x.invert(extent[1]) ])
+        line.select(".brush").call(brush.move, null) // This remove the grey brush area as soon as the selection has been done
+      }
+
+      // Update axis and line position
+      xAxis.transition().duration(1000).call(d3.axisBottom(x))
+      line
+          .select('.line')
+          .transition()
+          .duration(1000)
+          .attr("d", d3.line()
+            .x(function(d) { return x(d.date) })
+            .y(function(d) { return y(d.value) })
+          )
+    }
+
+    // If user double click, reinitialize the chart
+    svg.on("dblclick",function(){
+      x.domain(d3.extent(data, function(d) { return d.date; }))
+      xAxis.transition().call(d3.axisBottom(x))
+      line
+        .select('.line')
+        .transition()
+        .attr("d", d3.line()
+          .x(function(d) { return x(d.date) })
+          .y(function(d) { return y(d.value) })
+      )
+    });
 
 })
